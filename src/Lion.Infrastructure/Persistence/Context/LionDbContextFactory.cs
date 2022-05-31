124f6a05cf47c5cffd8f5e9fc945d08d13d180d9
Gustavo Rabanal <gustavo.rabanal.mendoza@gmail.com>
2022-05-31 13:20:20 -0500

    update

--- src/Lion.Infrastructure/Persistence/Context/LionDbContextFactory.cs
+++ src/Lion.Infrastructure/Persistence/Context/LionDbContextFactory.cs
@@ -7,19 +7,8 @@ namespace Lion.Infrastructure.Persistence.Context;
 [ExcludeFromCodeCoverage]
 public class LionDbContextFactory : IDesignTimeDbContextFactory<LionDbContext>
 {
-    public LionDbContext CreateDbContext(string connectionString)
-    {
-        return CreateDbContext(new string[] { connectionString });
-    }
-
     public LionDbContext CreateDbContext(string[] args)
     {
-        string connectionString = "server=any; port=3306; database=any; user=any; password=any;";
-        if (args.Length > 0)
-        {
-            connectionString = args[0];
-        }
-
         var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
         var builder = new DbContextOptionsBuilder<LionDbContext>();
         builder.UseMySql(serverVersion);
